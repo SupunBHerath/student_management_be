720adf73345d92ad27998830dc589c9cf02947c7
SupunBHerath <supunbherath@gmail.com>
2024-10-11 14:26:49 +0530

    connect to db

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-
+import express from 'express'
+import { getConnection } from './db/database_config.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-
+getConnection();
 app.listen(port, () => {
     console.log(`Server is running :${port}`);
 });
