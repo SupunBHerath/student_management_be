fb36ed76e30fb51c916bf1fe27319e2b64bff6a1
SupunBHerath <supunbherath@gmail.com>
2024-10-11 19:09:58 +0530

    fix  errror

--- controller/student_controller.js
+++ controller/student_controller.js
@@ -33,18 +33,20 @@ export const getByIdStudentDetails = async (req, res) => {
 };
 
 export const addStudentDetails = async (req, res) => {
+  
   let connection;
   try {
     connection = await dbConnection.getConnection();
-    const { name, contact, image_url } = req.body;
-
+    const { name, contact, image_url ,age } = req.body;
+    console.log(req.body);
+    
     if (!name || !contact) {
       res.status(500).send("name and contact are required");
     }
   
-    const query = `INSERT INTO student_details (name, contact, image_url) VALUES (?, ?, ?)`;
-    await connection.execute(query, [name, contact, image_url]);
-    res.status(201).json({ message: "Student added successfully", data: { name, contact, image_url } });
+    const query = `INSERT INTO student_details (name,age, contact , image_url) VALUES (?, ?, ?,?)`;
+    await connection.execute(query, [name, age, contact, image_url]);
+    res.status(201).json({ message: "Student added successfully", data: { name, age, contact, image_url } });
   } catch (err) {
     console.error("Error adding student details:", err);
     res.status(500).send("Error adding student details");
@@ -79,6 +81,8 @@ export const deleteStudentDetails = async (req, res) => {
   try {
     connection = await dbConnection.getConnection();
     const { id } = req.params;
+    console.log(id);
+    
     if(!id){
         res.status(500).json({message:"id is required"});
     }
--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.use('/student',studentRouter);
+app.use('/students',studentRouter);
 
 getConnection();
 app.listen(port, () => {
