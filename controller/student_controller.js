9b35952dca5c9634c59dee624b5ddd9f85e8e53f
SupunBHerath <supunbherath@gmail.com>
2024-10-11 15:34:44 +0530

    create delete , update , fun

--- controller/student_controller.js
+++ controller/student_controller.js
@@ -15,6 +15,22 @@ export const getAllStudentDetails = async (req, res) => {
 
   }
 };
+export const getByIdStudentDetails = async (req, res) => {
+  let connection;
+  const { id } = req.params;
+  try {
+    connection = await dbConnection.getConnection();
+    const query = `SELECT student_details where student_id=${id}`;
+    const [rows] = await connection.query(query);
+    res.json(rows);
+  } catch (err) {
+    console.error("Error fetching student details:");
+    res.status(500).send("Error fetching student details");
+  } finally {
+    if (connection) connection.release(); 
+
+  }
+};
 
 export const addStudentDetails = async (req, res) => {
   let connection;
@@ -37,3 +53,42 @@ export const addStudentDetails = async (req, res) => {
 
   }
 };
+
+export const updateStudentDetails = async (req, res) => {
+    let connection;
+  try {
+    connection = await dbConnection.getConnection();
+    const { id } = req.params;
+    const { name, contact, image_url } = req.body;
+    if(!id){
+        res.status(500).json({message:"id is required"});
+    }
+    const query = `UPDATE student_details SET name =?, contact =?, image_url =? WHERE student_id =?`;
+    await connection.execute(query, [name, contact, image_url, id]);
+    res.json({ message: "Student details updated successfully", data: { id, name, contact, image_url } });
+    } catch (err) {
+    console.error("Error updating student details:", err);
+    res.status(500).json({message:"Error updating student details"});
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
+export const deleteStudentDetails = async (req, res) => {
+    let connection;
+  try {
+    connection = await dbConnection.getConnection();
+    const { id } = req.params;
+    if(!id){
+        res.status(500).json({message:"id is required"});
+    }
+    const query = `DELETE FROM student_details WHERE student_id =?`;
+    await connection.execute(query, [id]);
+    res.json({ message: "Student details deleted successfully", id });
+    } catch (err) {
+    console.error("Error deleting student details:", err);
+    res.status(500).json({message:"Error deleting student details"});
+  } finally {
+    if (connection) connection.release();
+  }
+}
--- router/student_router.js
+++ router/student_router.js
@@ -1,9 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { addStudentDetails,getAllStudentDetails} from "../controller/student_controller.js"
+import { addStudentDetails,deleteStudentDetails,getAllStudentDetails, getByIdStudentDetails, updateStudentDetails} from "../controller/student_controller.js"
 
 router.get('/', getAllStudentDetails); 
+router.get('/:id',getByIdStudentDetails)
 router.post('/', addStudentDetails); 
+router.put('/:id', updateStudentDetails); 
+router.delete('/:id', deleteStudentDetails); 
 
 
 export default router;
