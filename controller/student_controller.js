46e67105f2388a29b40611bafd32db1a698791b8
SupunBHerath <supunbherath@gmail.com>
2024-10-11 15:17:11 +0530

    student create fun done

--- controller/student_controller.js
+++ controller/student_controller.js
@@ -1,17 +1,39 @@
-import dbConnection from '../db/database_config.js'
+import dbConnection from "../db/database_config.js";
 
-export const  getAllStudentDetails =  async (req , res )=>{
+export const getAllStudentDetails = async (req, res) => {
   let connection;
-  try{
-    connection = await dbConnection.getConnection()
+  try {
+    connection = await dbConnection.getConnection();
     const query = `SELECT * FROM student_details`;
     const [rows] = await connection.query(query);
     res.json(rows);
-  }catch(err){
-      console.error('Error fetching student details:');
-      res.status(500).send('Error fetching student details');
-  }finally{
-    connection.release();
-  }
+  } catch (err) {
+    console.error("Error fetching student details:");
+    res.status(500).send("Error fetching student details");
+  } finally {
+    if (connection) connection.release(); 
 
-}
+  }
+};
+
+export const addStudentDetails = async (req, res) => {
+  let connection;
+  try {
+    connection = await dbConnection.getConnection();
+    const { name, contact, image_url } = req.body;
+
+    if (!name || !contact) {
+      res.status(500).send("name and contact are required");
+    }
+  
+    const query = `INSERT INTO student_details (name, contact, image_url) VALUES (?, ?, ?)`;
+    await connection.execute(query, [name, contact, image_url]);
+    res.status(201).json({ message: "Student added successfully", data: { name, contact, image_url } });
+  } catch (err) {
+    console.error("Error adding student details:", err);
+    res.status(500).send("Error adding student details");
+  } finally {
+    if (connection) connection.release(); 
+
+  }
+};
--- router/student_router.js
+++ router/student_router.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import {getAllStudentDetails} from "../controller/student_controller.js"
+import { addStudentDetails,getAllStudentDetails} from "../controller/student_controller.js"
 
 router.get('/', getAllStudentDetails); 
+router.post('/', addStudentDetails); 
 
 
 export default router;
